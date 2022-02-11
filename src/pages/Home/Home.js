b54a8f3fca3414f64fe2ad3eb2e86ad0cc2615d8
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-11 13:06:32 -0300

    back to original commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function Home() {
   return (
     <>
-      <h1>LOGO1</h1>
+      <h1>LOGO</h1>
       <Link to="/Freelancers">
         <button>Freelancers</button>
       </Link>
