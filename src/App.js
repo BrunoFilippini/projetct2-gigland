ea37b6ce3a37d419542f656573fdeafc8d3f86bb
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-11 18:30:59 -0300

    formFreelancer working, sending to collection API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Employers } from "./pages/Employers/Employers";
 import { Freelancers } from "./pages/Freelancers/Freelancers";
+import { FormFreelancer } from "./pages/FormFreelancer/FormFreelancer";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Employers" element={<Employers />} />
         <Route path="/Freelancers" element={<Freelancers />} />
+        <Route path="/FormFreelancer" element={<FormFreelancer />} />
       </Routes>
     </BrowserRouter>
   );
