00fe781a73eae1149641e1152e214a0a77a4a7d0
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-13 23:18:07 -0300

    Navbar add with css and navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { NavBar } from "./components/NavBar/NavBar";
 import { Employers } from "./pages/Employers/Employers";
 import { Freelancers } from "./pages/Freelancers/Freelancers";
 import { FormFreelancer } from "./pages/FormFreelancer/FormFreelancer";
@@ -8,6 +9,7 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Employers" element={<Employers />} />
--- src/pages/Freelancers/Freelancers.js
+++ src/pages/Freelancers/Freelancers.js
@@ -1,7 +1,45 @@
+import { CardFreelancer } from "../../components/CardFreelancer/CardFreelancer";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export function Freelancers() {
+  const [freelancer, setFreelancer] = useState([]);
+
+  useEffect(() => {
+    async function fetchFreelancer() {
+      try {
+        const result = await axios.get(
+          "https://ironrest.herokuapp.com/giglandFreelas"
+        );
+        setFreelancer([...result.data]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchFreelancer();
+  }, []);
+
   return (
     <>
-      <h1>Freelancers List Page</h1>
+      <h1>Freelancers</h1>
+      {freelancer.map((currentFreelancer) => {
+        return (
+          <CardFreelancer
+            key={currentFreelancer._id}
+            id={currentFreelancer._id}
+            name={currentFreelancer.name}
+            profission={currentFreelancer.profission}
+            branding={currentFreelancer.branding}
+            education={currentFreelancer.education}
+            recentProjects={currentFreelancer.recentProjects}
+            skills={currentFreelancer.skills}
+            interest={currentFreelancer.interest}
+            contact={currentFreelancer.contact}
+            img={currentFreelancer.img}
+          />
+        );
+      })}
     </>
   );
 }
