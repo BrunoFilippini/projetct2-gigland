7f90249d0e8425bc958e572acb6606d9038e5c3e
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-15 20:52:52 -0300

    identação arrumada.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 export function Home() {
   return (
     <>
-      <h1>LOGO</h1>
-      <Link to="/Freelancers">
-        <button>Freelancers</button>
-      </Link>
+      <div className={styles.bg}>
+        <h1>LOGO</h1>
+        <Link to="/Freelancers">
+          <button>Freelancers</button>
+        </Link>
 
-      <Link to="/Employers">
-        <button>Employers</button>
-      </Link>
+        <Link to="/Employers">
+          <button>Employers</button>
+        </Link>
+      </div>
     </>
   );
 }
