99135d4348cbb26b55c9107b25f1cfe57e28fca8
Joe D <joedeejr@gmail.com>
2022-02-17 12:07:42 -0300

    SearchBar CSS

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,17 @@
+import styles from "./SearchBar.module.css";
+
 export function SearchBar(props) {
   return (
-    <input
-      type="text"
-      area="searchParams"
-      className="input search-bar"
-      placeholder="Find talent"
-      onKeyUp={(event) => {
+    <div className={styles.generalBar}>
+      <input
+        type="text"
+        area="searchParams"
+        className={styles.searchBar}
+        placeholder="Find talent"
+        onKeyUp={(event) => {
         props.filterAPI(event.target.value);
-      }}
-    />
+        }}
+      />
+    </div>
   );
 }
